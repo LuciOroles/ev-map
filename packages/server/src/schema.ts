66ccaa39fc49b13a1691d267cda29480b938b09f
LuciOroles <stoica.florin.lucian@gmail.com>
2023-04-29 22:38:30 +0300

    remove dummy data

--- packages/server/src/schema.ts
+++ packages/server/src/schema.ts
@@ -1,16 +1,14 @@
 import {
-  GraphQLSchema, GraphQLList,
-  GraphQLOutputType,
+  GraphQLSchema, 
+  GraphQLList,
   GraphQLID,
-  GraphQLNonNull, GraphQLInt, GraphQLObjectType, GraphQLString
+  GraphQLObjectType, 
+  GraphQLString,
 } from 'graphql';
-import answers from './ans.json';
-import categories from './category.json'
-import CompanyController  from './db/controllers';
 
+import CompanyController from './db/controllers';
 
 const { getAllCompanies } = CompanyController;
-
 
 const CompanyType: GraphQLObjectType<any, any> = new GraphQLObjectType({
   name: "Company",
@@ -28,60 +26,13 @@ const CompanyType: GraphQLObjectType<any, any> = new GraphQLObjectType({
   }),
 });
 
-
-var CategoryType: GraphQLOutputType = new GraphQLObjectType({
-  name: "category",
-  description: "a category",
-  fields: () => {
-    return {
-      id: {
-        type: GraphQLInt
-      },
-      name: {
-        type: new GraphQLNonNull(GraphQLString)
-      },
-      answers: {
-        type: new GraphQLList(AnswerType),
-        resolve: (parent) => {
-          return answers.ans.filter((element) => {
-            return element.code === parent['id']
-          })
-        }
-      }
-    };
-  }
-});
-
-var AnswerType: GraphQLObjectType<any, any> = new GraphQLObjectType({
-  name: "Answer",
-  description: "answer",
-  fields: {
-    code: {
-      type: GraphQLInt
-    },
-    label: {
-      type: new GraphQLNonNull(GraphQLString)
-    },
-    cateogryName: {
-      type: CategoryType,
-      resolve: (parent) => {
-        return categories.data.find((element) => {
-          return element.id === parent['code']
-        })
-      }
-    }
-  },
-
-});
-
-
-const RootAnsType = new GraphQLObjectType({
-  name: 'anwserList',
-  description: 'list of answers',
+const RootType = new GraphQLObjectType({
+  name: 'evRoot',
+  description: 'ev graph',
   fields: {
     companies: {
       type: new GraphQLList(CompanyType),
-      resolve:async ( ) => {
+      resolve: async () => {
         try {
           return (await getAllCompanies());
         } catch (error) {
@@ -89,37 +40,13 @@ const RootAnsType = new GraphQLObjectType({
           return [];
         }
       }
-    },
-    answer: {
-      type: AnswerType,
-      description: 'one answer',
-      args: {
-        id: {
-          type: GraphQLInt,
-        }
-      },
-      resolve: (parent, args) => {
-        return answers.ans.find((el) => el.code === args.id)
-      }
-    },
-    answers: {
-      type: new GraphQLList(AnswerType),
-      resolve: () => {
-        return answers.ans;
-      }
-    },
-    categories: {
-      type: new GraphQLList(CategoryType),
-      resolve: () => {
-        return categories.data;
-      }
     }
   }
 });
 
 
 const schema = new GraphQLSchema({
-  query: RootAnsType,
+  query: RootType,
 });
 
 export default schema;
