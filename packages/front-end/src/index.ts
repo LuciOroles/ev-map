c4cdce0503dc843cae405d8ba988265bf81252e5
LuciOroles <stoica.florin.lucian@gmail.com>
2023-04-30 21:23:32 +0300

    add fe dev mode with webpack

--- packages/front-end/src/index.ts
+++ packages/front-end/src/index.ts
@@ -1,3 +1,34 @@
-(()=> {
-    console.log('heello')
-})();
+ 
+import { GraphQLClient, gql } from 'graphql-request'
+
+async function run() {
+
+  const query = gql`
+  {
+    companies {
+      id
+     name
+     parent {id}
+    }
+  }
+`
+
+ 
+try {
+  const graphQLClient = new GraphQLClient('http://localhost:8000/graphql', {
+    mode: 'cors',
+    
+  });
+  const data = await graphQLClient.request(query)
+  console.log(data, ' data')
+} catch (error) {
+
+  console.error(error)
+}
+}
+
+  document.addEventListener("DOMContentLoaded", ()=> {
+    const comp =  document.getElementById('getCompanies');
+    fetch('http://localhost:8000/test')
+    comp.addEventListener('click', run);
+  });
