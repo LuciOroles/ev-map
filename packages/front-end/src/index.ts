691900344eb1ce2c3bd2404deae5ee0c4bc3605e
LuciOroles <stoica.florin.lucian@gmail.com>
2023-05-02 11:40:05 +0300

    get location on load

--- packages/front-end/src/index.ts
+++ packages/front-end/src/index.ts
@@ -1,12 +1,14 @@
 
 import  "./style/style.css";
 import { GraphQLClient } from 'graphql-request'
-import { getStation, allCompanies, allLocations } from './queries';
-import { renderMap } from "./map";
+import { allCompanies, allLocations } from './queries';
+import { handleMap } from "./map";
+import { Location } from "graphql";
 
  
 
-async function getLocations() {
+async function getLocations(): Promise<{'locations': Location[]}> {
+ 
   try {
     const graphQLClient = new GraphQLClient('http://localhost:8000/graphql', {
       mode: 'cors',
@@ -14,7 +16,7 @@ async function getLocations() {
     const data = await graphQLClient.request(allLocations);
     console.log(data, ' data')
  
-    renderMap( (data as Record<string, any>)['locations']);
+    return data as any;
   } catch (error) {
     console.log(error)
   }
@@ -34,9 +36,16 @@ async function getCompanies() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  // widget create;
+  // widget fill with data
+  const addressDetails = document.createElement('div');
+  addressDetails.style.position = "absolute";
+  ;
+  document.querySelector('#canvas').append(addressDetails);
   const comp = document.getElementById('getCompanies');
   comp.addEventListener('click', getCompanies);
-  const stGetter = document.getElementById('getStations');
-  stGetter.addEventListener('click', getLocations);
+  const stGetter = document.getElementById('getLocations');
+  const data = await getLocations();
+   handleMap(addressDetails)(data.locations as any)  ;
 });
--- packages/front-end/src/map/index.ts
+++ packages/front-end/src/map/index.ts
@@ -1,19 +1,19 @@
 import * as d3 from 'd3';
 import { getStationsByAddress } from '../api';
- 
 
 
-interface Station {
+// TOD0: type file
+export interface Station {
     id: string;
     name: string;
-    company_id:  string;
+    company_id: string;
     latitude: number;
     longitude: number;
     address: string;
     coord_key: string;
 }
 
-interface Location {
+export interface Location {
     latitude: number;
     longitude: number;
     address: string;
@@ -21,28 +21,35 @@ interface Location {
 
 
 
-export function renderMap(locations: Location[]) { 
- 
-    const container = d3.select('#canvas').append('svg')
-    .attr('width', '850px')
-    .attr('height', '850px');
 
-    for (const station of locations) {
-      const circle =  container
-          .append('circle')
-          .attr('cx', station.latitude)
-          .attr('cy', station.longitude)
-          .attr('r', 12)
-          .attr('fill',  'yellow')
-          .attr('class', 'y-circle');
-          circle.on('click', async () => {
-            await getStationsByAddress(station.address);
-          })
-        container.append('text')
-        .attr('x', station.latitude - 34 )
-        .attr('y', station.longitude < 50 ? station.longitude + 20 : station.longitude -20 )
-        .attr('fill', 'red')
-        .text(station.address);
+export function handleMap(note: HTMLDivElement) {
+
+    return function renderMap(locations: Location[]) {
+
+        const container = d3.select('#canvas').append('svg')
+            .attr('width', '850px')
+            .attr('height', '850px');
+
+        for (const station of locations) {
+            const circle = container
+                .append('circle')
+                .attr('cx', station.latitude)
+                .attr('cy', station.longitude)
+                .attr('r', 12)
+                .attr('fill', 'yellow')
+                .attr('class', 'y-circle');
+            circle.on('click', async (event: MouseEvent) => {
+
+                await getStationsByAddress(station.address);
+                const canvasCoords = document.querySelector("#canvas").getBoundingClientRect()
+                note.style.left = `${event.pageX - canvasCoords.x}px`;
+                note.style.top = `${event.pageY - canvasCoords.y}px`;
+                });
+            container.append('text')
+                .attr('x', station.latitude - 34)
+                .attr('y', station.longitude < 50 ? station.longitude + 20 : station.longitude - 20)
+                .attr('fill', 'red')
+                .text(station.address);
+        }
     }
-
 }
